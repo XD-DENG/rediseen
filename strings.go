e3209fafdbc9ac06909d17158dd8f2dc499d11da
XD-DENG <xd_deng@hotmail.com>
2019-10-21 22:51:40 +0800

    Allow configuring if expose the service to localhost only or 0.0.0.0
    Default will be "localhost"

--- strings.go
+++ strings.go
@@ -6,11 +6,11 @@ const strWrongTypeForIndexField = "wrong type for index/field"
 const strUsage = "Usage: ./rediseen [start/help/version]"
 const strHeader = "rediseen " + rediseenVersion
 
-const strHelpDoc = strHeader + "\n\n" +
-	strUsage + "\n\n" +
+const strHelpDoc = "\n\n" + strUsage + "\n\n" +
 	"Configuration Items (via environment variables):\n" +
-	"- REDISEEN_PORT: port of the service. Default port is 8000\n" +
 	"- REDISEEN_REDIS_URI: URI of your Redis database, e.g. `redis://:@localhost:6379`\n" +
+	"- REDISEEN_HOST: host of the service. Default port is 'localhost'\n" +
+	"- REDISEEN_PORT: port of the service. Default port is 8000\n" +
 	"- REDISEEN_DB_EXPOSED: Redis logical database(s) to expose. e.g., `0`, `0;3;9`, `0-9;15`, or `*`\n" +
 	"- REDISEEN_KEY_PATTERN_EXPOSED: Regular expression pattern, " +
 	"representing the name pattern of keys that you intend to expose\n" +
