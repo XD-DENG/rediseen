bd49a342c099e599ae146c21cfd204be4da7ac3e
Xiaodong <xd_deng@hotmail.com>
2019-10-29 12:45:15 +0800

    Add comment as golint suggests

--- types/types.go
+++ types/types.go
@@ -1,10 +1,12 @@
 package types
 
+// ResponseType acts as the JSON template for API response (successful calls)
 type ResponseType struct {
 	ValueType string      `json:"type"`
 	Value     interface{} `json:"value"`
 }
 
+// ErrorType acts as the JSON template for API response (failed calls)
 type ErrorType struct {
 	Error string `json:"error"`
 }
